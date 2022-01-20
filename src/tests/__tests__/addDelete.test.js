3e4490b2bd59c1224e27333df5ca6757117fd715
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-01-20 20:27:29 +0200

    Create a new file for the tests

--- src/tests/__tests__/addDelete.test.js
+++ src/tests/__tests__/addDelete.test.js
@@ -1,6 +1,6 @@
 /** * @jest-environment jsdom */
 import {
-  addtask, removeTask,updateStatuses, editTask, clearCompleted
+  addtask, removeTask
 } from '../addDelete';
 
 describe('Add and delete tests', () => {
@@ -9,15 +9,9 @@ describe('Add and delete tests', () => {
     index: 1,
     completed: false,
   };
-  const task2 = {
-    description: 'Task2',
-    index: 2,
-    completed: true,
-  };
 
   test('Add one item to the list', () => {
     addtask(task);
-    addtask(task2);
     const list = document.querySelectorAll('.listItem');
     expect(list).toHaveLength(1);
   });
@@ -27,22 +21,5 @@ describe('Add and delete tests', () => {
     const list = document.querySelectorAll('.listItem');
     expect(list).toHaveLength(0);
   });
-  
-  test ('Update a Task', () => {
-    const textUpdate = "This is the update";
-    editTask(task, textUpdate);
-    expect(task.description).toEqual("This is the update");
-  })
 
-  test('Update Status', () => {
-    const statusComplete = true;
-    updateStatuses(task, statusComplete);
-    expect(task.completed).toEqual(statusComplete);
-  })
-
-  test('clear completed tasks', () => {
-    clearCompleted();
-    const list = document.querySelectorAll('.listItem');
-    expect(list).toMatchObject({});
-  });
 });
