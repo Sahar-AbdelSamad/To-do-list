797d444ebc7f00c8b5dfe86a1198c9c6b5277b26
gdumani <gdumani@gmail.com>
2022-01-20 12:39:08 -0600

    add test for edit function

---
+++ src/tests/__tests__/clearcompleted-edit-status.test.js
@@ -0,0 +1,25 @@
+/** * @jest-environment jsdom */
+import {
+  updateStatuses, editTask, clearCompleted,
+} from '../addDelete';
+
+describe('Test clearCompleted tasks, update Status and edit Task', () => {
+  const task = {
+    description: 'Task1',
+    index: 1,
+    completed: false,
+  };
+
+  test('clear completed tasks', () => {
+    clearCompleted();
+    const list = document.querySelectorAll('.listItem');
+    expect(list).toMatchObject({});
+  });
+
+  test('Edit a Task', () => {
+    const textUpdate = 'This is the update';
+    expect(task.description).not.toEqual('This is the update');
+    editTask(task, textUpdate);
+    expect(task.description).toEqual('This is the update');
+  });
+});
